8ebf7990d58d6d0f3ad4f7195f27b15b7b0f602a
Andre Lejeune <andre_lejeune@msn.com>
2020-02-17 20:33:49 -0800

    Ajout de questions en medecine

---
+++ medhypogly.js
@@ -0,0 +1,52 @@
+const entete = "À quelle classe appartient ce médicament :";
+const nbr_choix = 4;
+data = [
+    {
+        "FIELD1":"Metformine",
+        "FIELD2":"Biguianide"
+    },
+    {
+        "FIELD1":"Linagliptine",
+        "FIELD2":"Inhibiteur DPP-4"
+    },
+    {
+        "FIELD1":"Saxagliptine",
+        "FIELD2":"Inhibiteur DPP-4"
+    },
+    {
+        "FIELD1":"Sitagliptine",
+        "FIELD2":"Inhibiteur DPP-4"
+    },
+    {
+        "FIELD1":"Gliclazide",
+        "FIELD2":"Sulfonylurées"
+    },
+    {
+        "FIELD1":"Glyburide",
+        "FIELD2":"Sulfonylurées"
+    },
+    {
+        "FIELD1":"Canagliflozine",
+        "FIELD2":"Inhibiteur SGLT-2"
+    },
+    {
+        "FIELD1":"Dapagliflozine",
+        "FIELD2":"Inhibiteur SGLT-2"
+    },
+    {
+        "FIELD1":"Empagliflozine",
+        "FIELD2":"Inhibiteur SGLT-2"
+    },
+    {
+        "FIELD1":"Dulaglutide",
+        "FIELD2":"Agoniste GLP-1"
+    },
+    {
+        "FIELD1":"Liraglutide",
+        "FIELD2":"Agoniste GLP-1"
+    },
+    {
+        "FIELD1":"Repaglinide",
+        "FIELD2":"Meglitinides"
+    }
+]
---
+++ medpompes.js
@@ -0,0 +1,84 @@
+const entete = "À quelle classe appartient ce médicament :";
+const nbr_choix = 4;
+data = [
+    {
+        "FIELD1":"Salmétérol",
+        "FIELD2":"B2 agoniste à longue durée d'action (LABA)"
+    },
+    {
+        "FIELD1":"Formotérol",
+        "FIELD2":"B2 agoniste à longue durée d'action (LABA)"
+    },
+    {
+        "FIELD1":"Indacatérol",
+        "FIELD2":"B2 agoniste à longue durée d'action (LABA)"
+    },
+    {
+        "FIELD1":"Ipratropium",
+        "FIELD2":"Anticholinergique à courte durée d'action (ACCA)"
+    },
+    {
+        "FIELD1":"Tiotropium",
+        "FIELD2":"Antagoniste muscarinique à longue durée d'action (ACLA)"
+    },
+    {
+        "FIELD1":"Glycopyrronium",
+        "FIELD2":"Antagoniste muscarinique à longue durée d'action (ACLA)"
+    },
+    {
+        "FIELD1":"Aclidinium",
+        "FIELD2":"Antagoniste muscarinique à longue durée d'action (ACLA)"
+    },
+    {
+        "FIELD1":"Uméclidinium",
+        "FIELD2":"Antagoniste muscarinique à longue durée d'action (ACLA)"
+    },
+    {
+        "FIELD1":"Fluticasone",
+        "FIELD2":"Corticostéroïde inhalé (CSI)"
+    },
+    {
+        "FIELD1":"Budésonide",
+        "FIELD2":"Corticostéroïde inhalé (CSI)"
+    },
+    {
+        "FIELD1":"Ciclésonide",
+        "FIELD2":"Corticostéroïde inhalé (CSI)"
+    },
+    {
+        "FIELD1":"Mométasone",
+        "FIELD2":"Corticostéroïde inhalé (CSI)"
+    },
+    {
+        "FIELD1":"Symbicort",
+        "FIELD2":"CSI + BALA"
+    },
+    {
+        "FIELD1":"Breo",
+        "FIELD2":"CSI + BALA"
+    },
+    {
+        "FIELD1":"Zenhale",
+        "FIELD2":"CSI + BALA"
+    },
+    {
+        "FIELD1":"Advair",
+        "FIELD2":"CSI + BALA"
+    },
+    {
+        "FIELD1":"Ultibro",
+        "FIELD2":"BALA + ACLA"
+    },
+    {
+        "FIELD1":"Inspiolto",
+        "FIELD2":"BALA + ACLA"
+    },
+    {
+        "FIELD1":"Combivent",
+        "FIELD2":"ACCA + BACA"
+    },
+    {
+        "FIELD1":"Trelegy",
+        "FIELD2":"ACLA + BALA + CSI"
+    }
+]
