1fb0ac342513c8c04cbf96dde0121ae53fef0d50
Andre Lejeune <andre_lejeune@msn.com>
2020-02-19 22:40:41 -0800

    Med - Ajout des bactéries

---
+++ medbacteries.js
@@ -0,0 +1,60 @@
+const entete = "À quelle groupe appartient cette bactérie :";
+const nbr_choix = 4;
+data = [
+    {
+        "FIELD1":"Staphylocoque",
+        "FIELD2":"Cocci gram +"
+    },
+    {
+        "FIELD1":"Streptocoque",
+        "FIELD2":"Cocci gram +"
+    },
+    {
+        "FIELD1":"Enterocoque",
+        "FIELD2":"Cocci gram +"
+    },
+    {
+        "FIELD1":"Neisseria",
+        "FIELD2":"Cocci gram -"
+    },
+    {
+        "FIELD1":"Moraxella",
+        "FIELD2":"Cocci gram -"
+    },
+    {
+        "FIELD1":"E. coli",
+        "FIELD2":"Bâtonnet gram négatif"
+    },
+    {
+        "FIELD1":"Pseudomonas",
+        "FIELD2":"Bâtonnet gram négatif"
+    },
+    {
+        "FIELD1":"Klebsiella",
+        "FIELD2":"Bâtonnet gram négatif"
+    },
+    {
+        "FIELD1":"Enterobacter",
+        "FIELD2":"Bâtonnet gram négatif"
+    },
+    {
+        "FIELD1":"Legionella",
+        "FIELD2":"Bâtonnet gram négatif"
+    },
+    {
+        "FIELD1":"Listeria",
+        "FIELD2":"Bâtonnet gram positif"
+    },
+    {
+        "FIELD1":"Nocardia",
+        "FIELD2":"Bâtonnet gram positif"
+    },
+    {
+        "FIELD1":"Actynomyces",
+        "FIELD2":"Bâtonnet gram positif"
+    },
+    {
+        "FIELD1":"Clostridium",
+        "FIELD2":"Bâtonnet gram positif"
+    }
+]
