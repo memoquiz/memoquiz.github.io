b4bd01593d791f18654f36ab4f52f4b8b287898e
cedbomb <cedric.lej@gmail.com>
2020-09-29 07:44:19 -0400

    ajout plus questions prob chances

--- probchances.js
+++ probchances.js
@@ -2,15 +2,34 @@ const entete = "Complétez le tableau suivant";
 data = [
         {
             "FIELD1": "Obtenir un RES lors d'un lancer de dé",
-            "FIELD2": ["1","3 ou un 5", "4, un 5 ou un 6"],
-            "FIELD3": [1,2,3],
+            "FIELD2": ["1","3 ou un 5", "4, un 5 ou un 6","chiffre pair","chiffre impair"],
+            "FIELD3": [1,2,3,3,3],
             "FIELD4": 6
 
         },
         {
-            "FIELD1": "Piger RES parmis 3 billes noires et 6 billes rouges ",
+            "FIELD1": "Piger RES parmis 3 billes noires et 6 billes rouges",
             "FIELD2": ["1 bille rouge", "1 bille noire"],
             "FIELD3": [6,3],
             "FIELD4": 9
+        },
+        {
+            "FIELD1": "Obtenir RES à pile ou face",
+            "FIELD2": ["pile", "face"],
+            "FIELD3": [1,1],
+            "FIELD4": 2
+        },
+        {
+            "FIELD1": "Sélectionner RES parmis les 52 cartes d'un paquet de cartes",
+            "FIELD2": ["un as", "une des 26 cartes noires", "un des deux rois rouges"],
+            "FIELD3": [1,26,2],
+            "FIELD4": 52
+        },
+        {
+            "FIELD1": "Obtenir un RES lors d'un lancer de dé à 12 faces",
+            "FIELD2": ["9","8 ou un 5", "4, un 5 ou un 6","chiffre pair","chiffre impair", "multiple de 3"],
+            "FIELD3": [1,2,6,6,4],
+            "FIELD4": 6
+
         }
     ]
