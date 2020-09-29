c74e2144ab6a7f73001fd474c32082434180d3ea
cedbomb <cedric.lej@gmail.com>
2020-09-28 20:52:43 -0400

    ajout calcul prob chances

---
+++ probchances.js
@@ -0,0 +1,16 @@
+const entete = "Complétez le tableau suivant";
+data = [
+        {
+            "FIELD1": "Obtenir un RES lors d'un lancer de dé",
+            "FIELD2": ["1","3 ou un 5", "4, un 5 ou un 6"],
+            "FIELD3": [1,2,3],
+            "FIELD4": 6
+
+        },
+        {
+            "FIELD1": "Piger RES parmis 3 billes noires et 6 billes rouges ",
+            "FIELD2": ["1 bille rouge", "1 bille noire"],
+            "FIELD3": [6,3],
+            "FIELD4": 9
+        }
+    ]
